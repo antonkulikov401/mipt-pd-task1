e4d4924f7ce12e12f79da005b01f92d900a9226d
Студент КАиТП 1-73 <pd89073@calc.cod.phystech.edu>
2020-09-23 18:07:41 +0300

    freqgher

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-for n in {3, 6, 8} do
-  for p in {1..8} do
+for n in 3 6 8 
+do
+  for p in {1..8} 
+  do
     sbatch -n $p --partition=RT --job-name="task1-$p-$n" --comment="task1" --output="out/out-$p-$n.txt" --error="error/error-$p-$n.txt" --wrap="mpiexec ./prog $p $n"
   done
 done
